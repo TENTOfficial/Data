a3aa2fd54f2a52ebc71e734291a35e962bc3c3bf
SnowGem <ceo@snowgem.org>
2019-01-20 13:39:13 +0700

    add download blockchain file

---
+++ blockchain.sh
@@ -0,0 +1,16 @@
+if [ ! -d ~/.snowgem ]; then
+  mkdir ~/.snowgem
+fi
+
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part1 -O ~/bc.sf-part1
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part2 -O ~/bc.sf-part2
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part3 -O ~/bc.sf-part3
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part4 -O ~/bc.sf-part4
+npm install
+node joinfile.js
+rm ~/bc.sf-part1
+rm ~/bc.sf-part2
+rm ~/bc.sf-part3
+rm ~/bc.sf-part4
+unzip -o ~/blockchain.zip -d ~/.snowgem
+rm ~/blockchain.zip
