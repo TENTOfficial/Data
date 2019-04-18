9b743776482acd8d4669748f5608a2ccb0c89b12
SnowGem <34765060+Snowgem@users.noreply.github.com>
2019-04-18 11:25:20 +0700

    Update blockchain.sh

--- blockchain.sh
+++ blockchain.sh
@@ -7,12 +7,10 @@ wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowg
 wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part3 -O bc.sf-part3
 wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part4 -O bc.sf-part4
 npm install
-if [ ! -f blockchain.zip ]; then
-  node joinfile.js
-  rm bc.sf-part1
-  rm bc.sf-part2
-  rm bc.sf-part3
-  rm bc.sf-part4
-  unzip -o blockchain.zip -d ~/.snowgem
-  rm blockchain.zip
-fi
+node joinfile.js
+rm bc.sf-part1
+rm bc.sf-part2
+rm bc.sf-part3
+rm bc.sf-part4
+unzip -o blockchain.zip -d ~/.snowgem
+rm blockchain.zip
