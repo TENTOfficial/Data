329d899461a7e69c5bae91902f8bf9870c4d2a56
SnowGem <34765060+Snowgem@users.noreply.github.com>
2019-05-23 16:17:14 +0700

    Update blockchain.sh

--- blockchain.sh
+++ blockchain.sh
@@ -6,15 +6,11 @@ sudo apt-get update
 sudo apt-get install \
       wget unzip curl libgomp1 -y
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install nodejs -y
-
 wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part1 -O bc.sf-part1
 wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part2 -O bc.sf-part2
 wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part3 -O bc.sf-part3
 wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part4 -O bc.sf-part4
-npm install
-node joinfile.js
+cat *.*part* > blockchain.zip
 rm bc.sf-part1
 rm bc.sf-part2
 rm bc.sf-part3
