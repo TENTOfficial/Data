9e838b8c7966cc684cde5097041b23cebb07125d
SnowGem <34765060+Snowgem@users.noreply.github.com>
2019-06-13 00:37:57 +0700

    update blockchain url

--- blockchain.sh
+++ blockchain.sh
@@ -6,10 +6,10 @@ sudo apt-get update
 sudo apt-get install \
       wget unzip curl libgomp1 -y
 
-wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part1 -O bc.sf-part1
-wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part2 -O bc.sf-part2
-wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part3 -O bc.sf-part3
-wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part4 -O bc.sf-part4
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_766300.zip.sf-part1 -O bc.sf-part1
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_766300.zip.sf-part2 -O bc.sf-part2
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_766300.zip.sf-part3 -O bc.sf-part3
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_766300.zip.sf-part4 -O bc.sf-part4
 cat *.*part* > blockchain.zip
 rm bc.sf-part1
 rm bc.sf-part2
