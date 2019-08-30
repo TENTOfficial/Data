f944ce66a1737e2d3bf453d34f3631739e6edddb
SnowGem <34765060+Snowgem@users.noreply.github.com>
2019-08-31 01:18:49 +0700

    Update redownload_blockchain.sh

--- redownload_blockchain.sh
+++ redownload_blockchain.sh
@@ -62,15 +62,18 @@ killall -9 snowgemd
 
 rm ~/.snowgem/blocks ~/.snowgem/chainstate -r
 
-wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_766300.zip.sf-part1 -O bc.sf-part1
-wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_766300.zip.sf-part2 -O bc.sf-part2
-wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_766300.zip.sf-part3 -O bc.sf-part3
-wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_766300.zip.sf-part4 -O bc.sf-part4
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_879228.zip.sf-part1 -O bc.sf-part1
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_879228.zip.sf-part2 -O bc.sf-part2
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_879228.zip.sf-part3 -O bc.sf-part3
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_879228.zip.sf-part4 -O bc.sf-part4
+wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index_879228.zip.sf-part5 -O bc.sf-part5
+
 cat *.*part* > blockchain.zip
 rm bc.sf-part1
 rm bc.sf-part2
 rm bc.sf-part3
 rm bc.sf-part4
+rm bc.sf-part5
 unzip -o blockchain.zip -d ~/.snowgem
 rm blockchain.zip
 
